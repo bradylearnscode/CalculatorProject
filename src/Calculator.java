8ae3d92ee689f3a317db4316d5d2bd53a6291151
Brady Whipple <bradywhip@gmail.com>
2024-01-01 22:54:18 -0700

    Commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,46 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args){
-        System.out.println("Hello World");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("First Number: ");
+        int firstNumber = scanner.nextInt();
+
+        System.out.println("Enter your Sign ( x , / , + , - ) ");
+        String sign = scanner.next();
+
+        System.out.println("Second Number: ");
+        int secondNumber = scanner.nextInt();
+
+        int total = 0;
+
+            if (sign.contains("+")){
+                total = firstNumber + secondNumber;
+        }
+            if (sign.contains("-")){
+                total = firstNumber - secondNumber;
+        }
+            if (sign.contains("/")){
+                total = firstNumber / secondNumber;
+        }
+            if (sign.contains("*")){
+                total = firstNumber * secondNumber;
+
+        }
+
+        System.out.println("Your answer is " + total);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -14,3 +53,4 @@ public class Calculator {
 
 
 }
+
